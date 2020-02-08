6d4685432a3b24209d56e77c03c1fa0c8de1f446
Xestra <47232518+Xestra1@users.noreply.github.com>
2020-02-08 11:23:55 -0500

    update #7
    - Meta tags
    - Header html

--- src/Landing.jsx
+++ src/Landing.jsx
@@ -349,7 +349,7 @@ class Landing extends React.Component {
             </div>
           </section>
           {/* 
-          * Contact
+          * Contact - finished
           */}
           <section className="section section-lg pt-lg-0 section-contact-us" id="contact">
             <Container>
@@ -411,68 +411,3 @@ class Landing extends React.Component {
 }
 
 export default Landing;
-
-
-{/* <h4 className="mb-1">Want to work with us?</h4>
-<p className="mt-0">
-  Your project is very important to us.
-</p>
-<FormGroup
-  className={classnames("mt-5", {
-    focused: this.state.nameFocused
-  })}
->
-  <InputGroup className="input-group-alternative">
-    <InputGroupAddon addonType="prepend">
-      <InputGroupText>
-        <i className="ni ni-user-run" />
-      </InputGroupText>
-    </InputGroupAddon>
-    <Input
-      placeholder="Your name"
-      type="text"
-      onFocus={e => this.setState({ nameFocused: true })}
-      onBlur={e => this.setState({ nameFocused: false })}
-    />
-  </InputGroup>
-</FormGroup>
-<FormGroup
-  className={classnames({
-    focused: this.state.emailFocused
-  })}
->
-  <InputGroup className="input-group-alternative">
-    <InputGroupAddon addonType="prepend">
-      <InputGroupText>
-        <i className="ni ni-email-83" />
-      </InputGroupText>
-    </InputGroupAddon>
-    <Input
-      placeholder="Email address"
-      type="email"
-      onFocus={e => this.setState({ emailFocused: true })}
-      onBlur={e => this.setState({ emailFocused: false })}
-    />
-  </InputGroup>
-</FormGroup>
-<FormGroup className="mb-4">
-  <Input
-    className="form-control-alternative"
-    cols="80"
-    name="name"
-    placeholder="Type a message..."
-    rows="4"
-    type="textarea"
-  />
-</FormGroup>
-<div>
-  <Button
-    block
-    className="btn-round"
-    color="primary"
-    size="lg"
-    type="button"
-  >
-    Send Message
-  </Button>
-</div> */}
